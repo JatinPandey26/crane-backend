0e5b4e0bb7c46c375111089ed7e84dd5be7d83bd
Jatin pandey <jatin26pandey@gmail.com>
2023-03-19 01:03:35 +0530

    backend

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../Models/user.model.js";
 export const verifyToken = async (req, res, next) => {
-  try {
+  try { 
     const token = req.cookies.access_token;
     if (!token) return res.status(401).send("Not authorized1");
     const { id } = await jwt.verify(token, process.env.JWT_SECRET);
